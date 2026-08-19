f4999e32d784801056697873144541c897e2099c
J_attaphon <52853486+J-attapon@users.noreply.github.com>
2026-08-19 16:52:37 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+window.RUBBER_CONFIG = {
+  SUPABASE_URL: 'https://jonrcmwxruyaulhyeyap.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_vgUx-ExLvrx3weS56qMz1A_6ioaRPzW',
+  AUTH_DOMAIN: 'gmail.com',
+  GRADES: ['3','4','5','ฟอง','คัดทิ้ง'],
+  VERSION: '8.0.0'
+};
